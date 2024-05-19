3785d5684549f43d21fa4ac2b50c925f3fe4c745
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-05-19 13:19:27 -0300

    feat: Adicionando funcionalidade de mostrar data e hora do sistema.

--- site_simples/app.js
+++ site_simples/app.js
@@ -3,16 +3,12 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click',() => {
         document.body.classList.toggle('dark-theme')
         var className = document.body.className;
-        if(className == "dark-theme") {
-            this.textContent = "Turn on lights";
-        } else {
-            this.textContent = "Turn off lights";
-        }
+        (className == "dark-theme") ? (this.textContent = "Turn on lights") : (this.textContent = "Turn off lights");
     }
 );
 
-const historyButton = document.querySelector('#history');
 const TARGET = "_blank";
+const historyButton = document.querySelector('#history');
 historyButton.addEventListener('click', () => {
     const linkHistory = "https://livramento.pb.gov.br/a_cidade/historia";
     window.open(linkHistory, TARGET);
@@ -40,3 +36,43 @@ const backToTop = document.querySelector('#backToTop');
 backToTop.addEventListener('click', () => {
     window.scrollTo(0, 0);
 });
+
+function updateTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+    const timeString = `${hours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+    document.getElementById('time').textContent = timeString;
+};
+setInterval(updateTime, 1000);
+  
+function updateDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.toLocaleString('default', { month: 'long' });
+    const day = now.getDate();
+    const dayOfWeek = now.toLocaleString('default', { weekday: 'long' });
+    const dateString = `${dayOfWeek} ${month} ${day}, ${year}`;
+    document.getElementById('date').textContent = dateString;
+};
+updateDate();
+  
+function updateGreeting() {
+    const now = new Date();
+    const hours = now.getHours();
+    let greeting;
+    if (hours < 6) {
+      greeting = 'Good night!';
+    } else if (hours < 12) {
+      greeting = 'Good morning!';
+    } else if (hours < 14) {
+      greeting = 'Good afternoon!';
+    } else {
+      greeting = 'Good evening!';
+    }
+    document.getElementById('greeting').textContent = greeting;
+  };
+updateGreeting();
